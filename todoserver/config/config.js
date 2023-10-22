a111578129da768069e543efd14b8001a6b4789e
55555-Jyeon <134191817+55555-Jyeon@users.noreply.github.com>
2023-10-22 23:50:26 +0900

    todoserver : backend data
    백엔드 통신 연습을 위해 임의로 만든 백엔드 데이터 
    (createdBy. Peter)

---
+++ todoserver/config/config.js
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const config = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_DEV_HOST,
+    dialect: "mysql",
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_DEV_HOST,
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_DEV_HOST,
+    dialect: "mysql",
+  },
+};
+export default config;
